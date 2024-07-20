94dc3a53dbc108ca4ffd9a2bd2c2998c07e8871d
kyllian-claveau <94695184+kyllian-claveau@users.noreply.github.com>
2024-07-20 21:23:30 +0200

    Add page restriction

--- src/Controller/User/Dashboard/DashboardController.php
+++ src/Controller/User/Dashboard/DashboardController.php
@@ -8,8 +8,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/user')]
+#[IsGranted('ROLE_USER')]
 class DashboardController extends AbstractController
 {
 #[Route('/dashboard', name: 'app_user_dashboard')]
