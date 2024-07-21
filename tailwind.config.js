9f2e1df5d8a530ab474b69b7aa08464107333392
kyllian-claveau <94695184+kyllian-claveau@users.noreply.github.com>
2024-07-22 01:31:32 +0200

    Change color for dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
+    color: {
+      primary: "#D53469",
+      secondary: "#DAAD51",
+    },
     extend: {},
   },
   plugins: [],
